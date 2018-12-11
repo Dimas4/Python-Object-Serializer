5009417fb7574f486d814ec6a10c2d406dae8eb1
Ivan <ivanshatukho@yandex.ru>
2018-12-11 15:33:39 +0300

    Fix absctract class

--- pyserializer/serializer/serializer.py
+++ pyserializer/serializer/serializer.py
@@ -1,33 +1,120 @@
+from pyserializer.field.field_type.funtion import Function
 from pyserializer.exception.exception import ManyError
+from pyserializer.validate.validate import Validate
 from .base.serializer import AbstractSerializer
 from pyserializer.field import field
 
 
 class BaseSerializer(AbstractSerializer):
-    @classmethod
-    def _get_obj_dict(cls, obj):
-        return super()._get_obj_dict(obj)
+    @staticmethod
+    def _get_obj_dict(obj):
+        """
+        Gets obj's attributes
+        :param obj:
+        :return: obj's attributes
+        """
+        return obj.__dict__
 
     @classmethod
     def _add_func_type_field(cls, _fields: dict, _field: str, _output: dict) -> bool or None:
-        return super()._add_func_type_field(_fields, _field, _output)
+        try:
+            if _fields[_field].type is Function:
+                if callable(_fields[_field]):
+                    _output[_field] = _fields[_field]()
+                elif hasattr(_fields[_field], 'func'):
+                    _output[_field] = _fields[_field].func()
+                else:
+                    _output[_field] = None
+                return True
+        except AttributeError:
+            pass
 
     @classmethod
     def _get_extra_field_with_func(cls, _output: dict, _fields: dict, _errors_extra_fields: set) -> None:
-        return super()._get_extra_field_with_func(_output, _fields, _errors_extra_fields)
+        """
+        Adds func type to _output
+        :param _output:
+        :param _fields:
+        :param _errors_extra_fields:
+        :return: _output
+        """
+        _prepared = dict(_output)
+
+        for _field in set(_errors_extra_fields):
+            if _field.startswith('get_'):
+                _errors_extra_fields.remove(_field)
+
+            _get_field_func = _fields.get(f'get_{_field}')
+            if _get_field_func:
+                _errors_extra_fields.remove(_field)
+                _output[_field] = _get_field_func(_prepared)
+                continue
+
+            if cls._add_func_type_field(_fields, _field, _output):
+                _errors_extra_fields.remove(_field)
 
     @classmethod
     def _get_obj_fields_and_errors(cls, obj, _fields: dict) -> tuple:
-        return super()._get_obj_fields_and_errors(obj, _fields)
+        """
+        Gets all the specific fields of an obj
+        :param obj:
+        :param _fields:
+        :return: The specific fields of an obj
+        """
+        _object_field_set = set(obj)
+        _obj_wrong_type_fields = set()
+        _obj_wrong_params_field = [{} for _ in range(len(obj))]
+        _output = {}
 
-    @classmethod
-    def _is_iter(cls, obj) -> bool:
-        return super()._is_iter(obj)
+        for ind, (_key, _value) in enumerate(obj.items()):
+            if _key in _fields:
+                if not isinstance(_fields[_key].type, type) or not isinstance(_value, _fields[_key].type):
+                    _obj_wrong_type_fields.add(_key)
+                    continue
+
+                _validate_errors = Validate.validate_errors(_fields[_key], _value)
+                for _error in _validate_errors:
+                    _obj_wrong_params_field[ind].update(_error)
+
+                _output[_key] = _value
+
+        _errors_extra_fields = set(_fields) - _object_field_set
+
+        cls._get_extra_field_with_func(_output, _fields, _errors_extra_fields)
+
+        return _output, _errors_extra_fields, _obj_wrong_type_fields, _obj_wrong_params_field
+
+    @staticmethod
+    def _is_iter(obj) -> bool:
+        """
+        Checks if the obj is iterable
+        :param obj:
+        :return: True if obj is iterable, else False
+        """
+        try:
+            iter(obj)
+            return True
+        except TypeError:
+            return False
 
     @classmethod
     def _add_error_info(cls, _output: dict, **kwargs) -> None:
-        return super()._add_error_info(_output, **kwargs)
-
+        """
+        Add errors info to _output
+        :param _output:
+        :param _errors_extra_fields:
+        :param _errors_wrong_type_fields:
+        :return: _output with errors info
+        """
+        _output['errors'] = {}
+        for key, value in kwargs.items():
+            if isinstance(value, list):
+                for ind, element in enumerate(value):
+                    if element:
+                        _output['errors'].update({list(_output.items())[ind][0]: dict(element.items())})
+                continue
+            if value:
+                _output['errors'].update({key: list(value)})
 
     @classmethod
     def _serialize_many(cls, obj, _fields: dict) -> list:
